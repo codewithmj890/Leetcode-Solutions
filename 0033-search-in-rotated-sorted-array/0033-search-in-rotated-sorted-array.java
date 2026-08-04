5708d0759ba5214022b893af4dc7a7b59d5099fd
MRITYUNJAY SINGH <codewithmj890@gmail.com>
2026-08-04 21:23:49 +0530

    Time: 0 ms (100%), Space: 43.8 MB (65.94%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,3 +1,24 @@
+class Main {
+    public static void main(String[] args) {
+        Solution solver = new Solution();
+        
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        
+        // Test 1: Target exists in the right (rotated) half
+        System.out.println("Find 0 (Expected 4): " + solver.search(nums, 0));
+        
+        // Test 2: Target exists in the left (unrotated) half
+        System.out.println("Find 5 (Expected 1): " + solver.search(nums, 5));
+        
+        // Test 3: Target does not exist
+        System.out.println("Find 3 (Expected -1): " + solver.search(nums, 3));
+        
+        // Test 4: Edge case of array size 1
+        int[] single = {1};
+        System.out.println("Find 1 in [1] (Expected 0): " + solver.search(single, 1));
+    }
+}
+
 class Solution {
     public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
@@ -9,16 +30,13 @@ class Solution {
                 return mid;
             }
             
-            // Left half is sorted
             if (nums[left] <= nums[mid]) {
                 if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-            } 
-            // Right half is sorted
-            else {
+            } else {
                 if (nums[mid] < target && target <= nums[right]) {
                     left = mid + 1;
                 } else {
