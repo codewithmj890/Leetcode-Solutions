f13e42b8aa614f93ee246591d6df03920385a78a
MRITYUNJAY SINGH <codewithmj890@gmail.com>
2026-09-05 21:11:29 +0530

    Time: 7 ms (72.51%), Space: 49.7 MB (59.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Main {
+    public static void main(String[] args) {
+        Solution solver = new Solution();
+        
+        // Test 1: The standard example
+        String[] test1 = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println("Test 1: " + solver.groupAnagrams(test1));
+        // Expected: [[eat, tea, ate], [bat], [tan, nat]] (Order of groups doesn't matter)
+        
+        // Test 2: Empty strings
+        String[] test2 = {""};
+        System.out.println("Test 2: " + solver.groupAnagrams(test2));
+        // Expected: [[""]]
+        
+        // Test 3: Single characters
+        String[] test3 = {"a"};
+        System.out.println("Test 3: " + solver.groupAnagrams(test3));
+        // Expected: [["a"]]
+    }
+}
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
